64aa64c3e4cc077c70cb6d8126e08e906b426a06
brazorf <brazorf@users.noreply.github.com>
2016-10-09 02:58:14 +0200

    chore: codeclimate test reporter.

--- src/config/visits.php
+++ src/config/visits.php
@@ -5,13 +5,13 @@ return [
     "enabled" => true,
 
     // Add patterns to be blacklisted (ignored and not tracked).
+    // These patterns will apply if the UrlWhitelist rule is
+    // enabled in the chain.
     "blacklist" => [
         // i.e. '/^(admin|api|auth).*/'
     ],
 
-    // Add patterns to be blacklisted (ignored and not tracked).
-    // These patterns will apply if the UrlWhitelist rule is
-    // enabled in the chain.
+    // Rules chain.
     "rules" => [
         "Foothing\Laravel\Visits\Rules\Crawler",
         "Foothing\Laravel\Visits\Rules\UrlWhitelist",
